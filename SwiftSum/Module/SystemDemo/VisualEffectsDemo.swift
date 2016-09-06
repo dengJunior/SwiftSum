b887887b06d8f5e24310c99f7060e9eb6207ba93
sihuan <cgcym1234@163.com>
2016-09-06 11:12:05 +0800

    CoreAnimation add  4-视觉效果 demo 完善

--- SwiftSum/Module/SystemDemo/VisualEffectsDemo.swift
+++ SwiftSum/Module/SystemDemo/VisualEffectsDemo.swift
@@ -16,6 +16,10 @@ class VisualEffectsDemo: UIViewController {
     @IBOutlet weak var imageViewOrigin: UIImageView!
     @IBOutlet weak var imageViewOriginMasked: UIImageView!
     
+    @IBOutlet weak var kCAFilterOriginView: UIImageView!
+    @IBOutlet weak var kCAFilterLinerView: UIView!
+    @IBOutlet weak var kCAFilterNearestView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         cornerRadiusDemo()
@@ -23,6 +27,7 @@ class VisualEffectsDemo: UIViewController {
         shadowDemo()
         shadowPathDemo()
         maskDemo()
+        kCAFilterDemo()
     }
     
     // MARK: - 1. cornerRadius
@@ -108,6 +113,35 @@ class VisualEffectsDemo: UIViewController {
         imageViewOriginMasked.layer.mask = maskLayer
     }
     
+    // MARK: - 7. 拉伸过滤 minificationFilter magnificationFilter
+    /**
+     当我们视图显示一个图片的时候，都应该正确地显示这个图片（意即：以正确的比例和正确的1：1像素显示在屏幕上）。原因如下：
+     
+     - 能够显示最好的画质，像素既没有被压缩也没有被拉伸。
+     - 能更好的使用内存，因为这就是所有你要存储的东西。
+     - 最好的性能表现，CPU不需要为此额外的计算。
+     
+     当图片需要显示不同的大小的时候，有一种叫做拉伸过滤的算法就起到作用了。它作用于原图的像素上并根据需要生成新的像素显示在屏幕上。
+     
+     事实上，重绘图片大小也没有一个统一的通用算法。这取决于需要拉伸的内容，放大或是缩小的需求等这些因素。CALayer为此提供了三种拉伸过滤方法，他们是：
+     
+     - kCAFilterLinear
+     - kCAFilterNearest
+     - kCAFilterTrilinear
+     */
+    func kCAFilterDemo() {
+        /**
+         kCAFilterTrilinear和kCAFilterLinear非常相似，大部分情况下二者都看不出来有什么差别。但是，较双线性滤波算法而言，三线性滤波算法存储了多个大小情况下的图片（也叫多重贴图），并三维取样，同时结合大图和小图的存储进而得到最后的结果。
+         */
+        kCAFilterLinerView.layer.contents = kCAFilterOriginView.image?.CGImage
+        kCAFilterLinerView.layer.magnificationFilter = kCAFilterLinear
+        
+        /**
+         kCAFilterNearest是一种比较武断的方法。从名字不难看出，这个算法（也叫最近过滤）就是取样最近的单像素点而不管其他的颜色。这样做非常快，也不会使图片模糊。但是，最明显的效果就是，会使得压缩图片更糟，图片放大之后也显得块状或是马赛克严重。
+         */
+        kCAFilterNearestView.layer.contents = kCAFilterOriginView.image?.CGImage
+        kCAFilterNearestView.layer.magnificationFilter = kCAFilterNearest
+    }
 }
 
 
